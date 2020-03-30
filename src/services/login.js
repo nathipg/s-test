f3c58d053c5f15f54a33fc5fdf9e46a6a55558ec
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-30 15:24:39 -0300

    Removindo localStorage test

--- src/services/login.js
+++ src/services/login.js
@@ -1,12 +1,11 @@
 export const login = async () => {
-    window.localStorage.setItem('userId', 1);
-    window.localStorage.setItem('userName', 'Nathália Pissuti');
+    // Logim
 };
 
 export const getId = () => {
-    return window.localStorage.getItem('userId');
+    return 1;
 };
 
 export const getName = () => {
-    return window.localStorage.getItem('userName');
+    return 'Nathália Pissuti';
 };
